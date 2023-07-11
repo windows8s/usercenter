6abffb904fc8ed16ccc21c7e04bf105f7a89fb97
windows8s <867313797@qq.com>
2023-07-12 07:48:44 +0800

    user-center-backend

--- src/main/java/com/gdgd/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/gdgd/usercenter/service/impl/UserServiceImpl.java
@@ -99,7 +99,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         user.setPlanetCode(planetCode);
         boolean saveResult = this.save(user);
         if (!saveResult) {
-            return -1;
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "保存失败");
         }
         return user.getId();
 
@@ -110,22 +110,22 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     public User userLogin(String userAccount, String userPassword, HttpServletRequest request) {
         //1. 校验
         if (StringUtils.isAnyBlank(userAccount, userPassword)) {
-            return null;
+            throw new BusinessException(ErrorCode.NULL_ERROR, "账号或密码为空");
         }
 
         if (userAccount.length() < 4) {
-            return null;
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号长度小于4位");
         }
 
         if (userPassword.length() < 8) {
-            return null;
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "密码长度小于8位");
         }
 
         //账户不包含特殊字符
         String validPattern = "[`~!@#$%^&*()+=|{}':;',\\\\[\\\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
         Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
         if (matcher.find()) {
-            return null;
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "含有特殊字符");
         }
 
         // 2.加密
@@ -135,11 +135,12 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("userAccount", userAccount);
         queryWrapper.eq("userPassword", encryptPassword);
+
         User user = userMapper.selectOne(queryWrapper);
         //用户不存在
         if (user == null) {
             log.info("user login failed,userAccount cannot match userPassword...");
-            return null;
+            throw new BusinessException(ErrorCode.NULL_ERROR, "用户不存在");
         }
 
         //3.用户脱敏,防止数据库的字段泄露给前端
@@ -186,7 +187,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     public int userLogout(HttpServletRequest request) {
         //移除登录态
         request.getSession().removeAttribute(USER_LOGIN_STATE);
-
         //todo 待优化
         return 1;
 
